febb97befea50a7882a83760bf61d1e5c428c693
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-10 15:22:52 +0300

    Classes and Inheritance In C sharp

---
+++ C# - Level 1/Main1/Main1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//this is a library.
+using System;
+
+class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("Animal");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Dog");
+    }
+    
+    static void Main()
+    {
+        
+        Dog d = new Dog();
+        d.Speak();
+        
+    }
+}
