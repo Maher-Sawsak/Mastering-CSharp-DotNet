8eccd43881a42e2b4ab7981af193534fcd417f45
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-11 14:59:43 +0300

    Formating String

---
+++ C# - Level 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        //Formated string.
+        Console.WriteLine("{0} {1}", "Maher", "Sawsak");
+        Console.WriteLine("{0} {1} {2}", "Name : ", "Maher", "Sawsak");
+    }
+
+}
