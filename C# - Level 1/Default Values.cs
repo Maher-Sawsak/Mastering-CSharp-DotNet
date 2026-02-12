cb9f2058f0cafda49ab56716128e4a1544576cdc
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-12 09:26:32 +0300

    Default for variables in C#

---
+++ C# - Level 1/Default Values.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //using default(type)
+            int i = default(int); 
+            float f = default(float);
+            decimal d = default(decimal);
+            bool b = default(bool);//will be false.
+            char c = default(char);
+
+            // C# 7.1 onwards
+            //get default value using just default 
+            int i2 = default; 
+            float f2 = default;
+            decimal d2 = default;
+            bool b2 = default;
+            char c2 = default;
+
+        }
+    }
+}
