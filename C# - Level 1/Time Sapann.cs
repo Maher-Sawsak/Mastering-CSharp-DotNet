3ae407e3993e10097af1342c1ea3c9f7abde1363
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-12 11:13:34 +0300

    New Concept TimeSpan in C#

---
+++ C# - Level 1/Time Sapann.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Span
+{
+    internal class TimeSpann
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime d = new DateTime(2026,02,12);
+
+            TimeSpan ts = new TimeSpan(25, 5, 30);
+
+
+            //it will return how much remain after calculating the day.
+            Console.WriteLine(ts);
+            Console.WriteLine(ts.Days);
+            Console.WriteLine(ts.Minutes);
+            Console.WriteLine(ts.Hours);//1 hours like 24 -25 =1
+
+
+
+            //new date storing the d + time span ts
+            DateTime newDate = d.Add(ts);
+            Console.WriteLine(newDate);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
