1814ef7e5a2b8b334e6f52f6a7924e3b572ec9c7
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-12 18:09:15 +0300

    New : new syntax in C# for reading string

---
+++ C# - Level 1/Read Line Syntax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadLine_Syntax
+{
+    internal class ReadLineSyntax
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name : ");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("The Name You Entered Is  : "  + Name);
+
+            Console.WriteLine($"Hello {Name}, How are u doing !!");
+        }
+    }
+}
