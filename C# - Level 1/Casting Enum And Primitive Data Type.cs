1c24104f4a8f7893f0c143690a2fe90728052b37
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-12 17:58:23 +0300

    doc : Syntax for casting in C#

---
+++ C# - Level 1/Casting Enum And Primitive Data Type.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+
+namespace Main
+{
+    internal class Program
+    {
+        enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+
+
+
+            int SmallerOne = 2;
+            double BiggerOne = 3.42;
+
+
+
+            Console.WriteLine($"The Value Of Converting From double to int    {(int)BiggerOne}  :   " );
+            Console.WriteLine($"\nThe Value Of Converting From int to double   {SmallerOne} :   ");
+                                                                                                        
+                
+
+
+            Console.WriteLine(WeekDays.Friday);
+
+            //for Casting the enum .
+            int day = (int)WeekDays.Friday;    
+            Console.WriteLine(day);            
+
+            var wd = (WeekDays)5; 
+            Console.WriteLine(wd);
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
