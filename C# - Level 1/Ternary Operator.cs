33238d36a6af74d4601ffa7d9d2cce10e70a81a6
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-13 08:35:49 +0300

    Ternary Operator In C# Syntax Example

---
+++ C# - Level 1/Ternary Operator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsEven;
+
+            Console.WriteLine("Please Enter A Number To Check Even Or Not   ? ");
+
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            IsEven = (Number % 2 == 0) ? true : false;
+
+            Console.WriteLine((Number % 2 == 0) ? "Yes" : "No");
+
+        }        
+    }
+}
