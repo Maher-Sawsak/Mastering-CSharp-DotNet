695a732f5d904e51362bc4e752bf93f523cd3839
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-13 15:14:51 +0300

    Random Number for Some Cases (in Range/To certian max/Without Range)

---
+++ C# - Level 1/Random Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Random_Number_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(2));//here is the max.
+            Console.WriteLine(rnd.Next(1,10));//this in range from min to max.
+            Console.WriteLine(rnd.Next());//this will return a random Number just positive.
+        }
+    }
+}
