b8cde559bcfba71cc7dcc55205d823ee3358ba15
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-18 22:16:52 +0300

    Exersice To-Do List By checked list box

---
+++ C# - Level 1/CheckedListBox/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace CheckedListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ckslbTasks = new System.Windows.Forms.CheckedListBox();
+            this.lable1 = new System.Windows.Forms.Label();
+            this.btnAdd = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.btnDelete = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.txtb1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ImageButton1 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.guna2ImageButton2 = new Guna.UI2.WinForms.Guna2ImageButton();
+            this.lblDisplaytxt = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.SuspendLayout();
+            // 
+            // ckslbTasks
+            // 
+            this.ckslbTasks.BackColor = System.Drawing.Color.Cornsilk;
+            this.ckslbTasks.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ckslbTasks.FormattingEnabled = true;
+            this.ckslbTasks.Location = new System.Drawing.Point(414, 86);
+            this.ckslbTasks.Name = "ckslbTasks";
+            this.ckslbTasks.Size = new System.Drawing.Size(374, 209);
+            this.ckslbTasks.TabIndex = 0;
+            this.ckslbTasks.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // lable1
+            // 
+            this.lable1.AutoSize = true;
+            this.lable1.BackColor = System.Drawing.Color.LightYellow;
+            this.lable1.Font = new System.Drawing.Font("Palace Script MT", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lable1.Location = new System.Drawing.Point(434, 25);
+            this.lable1.Name = "lable1";
+            this.lable1.Size = new System.Drawing.Size(175, 43);
+            this.lable1.TabIndex = 1;
+            this.lable1.Text = "To Do List";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btnAdd.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.ImageOffset = new System.Drawing.Point(0, 0);
+            this.btnAdd.ImageRotate = 0F;
+            this.btnAdd.ImageSize = new System.Drawing.Size(60, 60);
+            this.btnAdd.Location = new System.Drawing.Point(288, 52);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btnAdd.Size = new System.Drawing.Size(69, 66);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btnDelete.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
+            this.btnDelete.ImageOffset = new System.Drawing.Point(0, 0);
+            this.btnDelete.ImageRotate = 0F;
+            this.btnDelete.ImageSize = new System.Drawing.Size(62, 62);
+            this.btnDelete.Location = new System.Drawing.Point(693, 326);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.btnDelete.Size = new System.Drawing.Size(73, 71);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtb1
+            // 
+            this.txtb1.Animated = true;
+            this.txtb1.BorderRadius = 23;
+            this.txtb1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtb1.DefaultText = "";
+            this.txtb1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtb1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtb1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtb1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtb1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtb1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtb1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtb1.Location = new System.Drawing.Point(12, 52);
+            this.txtb1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtb1.Name = "txtb1";
+            this.txtb1.PlaceholderText = "";
+            this.txtb1.SelectedText = "";
+            this.txtb1.Size = new System.Drawing.Size(270, 58);
+            this.txtb1.TabIndex = 6;
+            this.txtb1.TextChanged += new System.EventHandler(this.txtb1_TextChanged);
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.Animated = true;
+            this.guna2Button1.BorderRadius = 40;
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.Green;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(315, 331);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(98, 74);
+            this.guna2Button1.TabIndex = 7;
+            this.guna2Button1.Text = "Show Checked";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2ImageButton1
+            // 
+            this.guna2ImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ImageButton1.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageButton1.Image")));
+            this.guna2ImageButton1.ImageOffset = new System.Drawing.Point(0, 0);
+            this.guna2ImageButton1.ImageRotate = 0F;
+            this.guna2ImageButton1.Location = new System.Drawing.Point(587, 331);
+            this.guna2ImageButton1.Name = "guna2ImageButton1";
+            this.guna2ImageButton1.PressedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton1.Size = new System.Drawing.Size(69, 66);
+            this.guna2ImageButton1.TabIndex = 8;
+            this.guna2ImageButton1.Click += new System.EventHandler(this.guna2ImageButton1_Click);
+            // 
+            // guna2ImageButton2
+            // 
+            this.guna2ImageButton2.AnimatedGIF = true;
+            this.guna2ImageButton2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ImageButton2.CheckedState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton2.HoverState.ImageSize = new System.Drawing.Size(64, 64);
+            this.guna2ImageButton2.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageButton2.Image")));
+            this.guna2ImageButton2.ImageOffset = new System.Drawing.Point(0, 0);
+            this.guna2ImageButton2.ImageRotate = 0F;
+            this.guna2ImageButton2.ImageSize = new System.Drawing.Size(120, 170);
+            this.guna2ImageButton2.Location = new System.Drawing.Point(470, 321);
+            this.guna2ImageButton2.Name = "guna2ImageButton2";
+            this.guna2ImageButton2.PressedState.ImageSize = new System.Drawing.Size(100, 100);
+            this.guna2ImageButton2.Size = new System.Drawing.Size(76, 66);
+            this.guna2ImageButton2.TabIndex = 9;
+            this.guna2ImageButton2.Click += new System.EventHandler(this.guna2ImageButton2_Click);
+            // 
+            // lblDisplaytxt
+            // 
+            this.lblDisplaytxt.BackColor = System.Drawing.Color.White;
+            this.lblDisplaytxt.Enabled = false;
+            this.lblDisplaytxt.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplaytxt.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblDisplaytxt.Location = new System.Drawing.Point(35, 70);
+            this.lblDisplaytxt.Name = "lblDisplaytxt";
+            this.lblDisplaytxt.Size = new System.Drawing.Size(159, 24);
+            this.lblDisplaytxt.TabIndex = 10;
+            this.lblDisplaytxt.Text = "Please Write here...     \r\n";
+            this.lblDisplaytxt.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(470, 403);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(71, 21);
+            this.guna2HtmlLabel1.TabIndex = 11;
+            this.guna2HtmlLabel1.Text = "Check All";
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(708, 402);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(49, 21);
+            this.guna2HtmlLabel2.TabIndex = 12;
+            this.guna2HtmlLabel2.Text = "Delete";
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel3.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(310, 411);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(103, 21);
+            this.guna2HtmlLabel3.TabIndex = 13;
+            this.guna2HtmlLabel3.Text = "Show Checked";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.guna2HtmlLabel3);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.lblDisplaytxt);
+            this.Controls.Add(this.guna2ImageButton2);
+            this.Controls.Add(this.guna2ImageButton1);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.txtb1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lable1);
+            this.Controls.Add(this.ckslbTasks);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox ckslbTasks;
+        private System.Windows.Forms.Label lable1;
+        private Guna.UI2.WinForms.Guna2ImageButton btnAdd;
+        private Guna.UI2.WinForms.Guna2ImageButton btnDelete;
+        private Guna.UI2.WinForms.Guna2TextBox txtb1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton1;
+        private Guna.UI2.WinForms.Guna2ImageButton guna2ImageButton2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblDisplaytxt;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+    }
+}
+
---
+++ C# - Level 1/CheckedListBox/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckedListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ckslbTasks.Items.Add(txtb1.Text);
+        }
+
+        private void txtb1_TextChanged(object sender, EventArgs e)
+        {
+            lblDisplaytxt.Visible = false;
+        }
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            ckslbTasks.Items.RemoveAt(0);
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ckslbTasks.CheckedItems.Count; i++)
+            {
+                MessageBox.Show(ckslbTasks.CheckedItems[i].ToString(), "Checked Item");
+            }
+        }
+
+        private void guna2ImageButton1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ckslbTasks.Items.Count; i++) {
+
+                MessageBox.Show(ckslbTasks.Items[i].ToString());
+            
+            }
+            /*or we can use foreach also.
+            foreach (var item in ckslbTasks.Items) {
+                MessageBox.Show(item.ToString());
+            }
+            */
+        }
+
+        private void guna2ImageButton2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ckslbTasks.Items.Count; i++) {
+                ckslbTasks.SetItemChecked(i, true);
+            
+            }
+        }
+    }
+}
