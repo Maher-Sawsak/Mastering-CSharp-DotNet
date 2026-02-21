80c8cbcf2d5899d0717e5fc195c70601a4d289a5
Maher-Sawsak <mahersawsak2@gmail.com>
2026-02-22 00:14:34 +0300

    New Concept :   MDI Container for Forms

---
+++ C# - Level 1/Multiple Documents Interface (MDI) Container/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiple_Documents_Interface__MDI__Container
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmEmp Emp = new frmEmp();
+            Emp.MdiParent = this;
+            Emp.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmDep Dep = new frmDep();
+            Dep.MdiParent = this;
+            Dep.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmSalary Salaray = new frmSalary();
+            Salaray.MdiParent = this;
+            Salaray.Show();
+        }
+    }
+}
