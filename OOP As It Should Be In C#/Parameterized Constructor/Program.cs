17832031fb770ec9b5dd0e4b4c4e5fce45fb3578
Maher-Sawsak <mahersawsak2@gmail.com>
2026-04-18 19:45:23 +0300

    Add Parameterized Constructor examples

---
+++ OOP As It Should Be In C#/Parameterized Constructor/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Parameterized_Constructor
+{
+    class clsPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        
+
+        public clsPerson(int ID , string Name , int Age){
+
+            this.Id = ID;
+            this.Name = Name;
+            this.Age = Age;        
+        }
+
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsPerson Person1 = new clsPerson(10, "Maher Sawsak", 21);
+
+            Console.WriteLine(Person1.Id);
+            Console.WriteLine(Person1.Name);
+            Console.WriteLine(Person1.Age);
+
+        }
+    }
+}
