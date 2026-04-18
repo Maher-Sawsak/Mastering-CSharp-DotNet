c8854d0a5ce63576c6b2d8b418a5c732239fd339
Maher-Sawsak <mahersawsak2@gmail.com>
2026-04-18 19:45:16 +0300

    Add Auto Implemented Properties examples

---
+++ OOP As It Should Be In C#/Auto Implemented Properties/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Auto_Implemented_Properties
+{
+
+//So Auto implemented properties are helpful,
+//when you don't think you need any validation, computation or any implementation.
+
+    class clsEmployee {
+
+        //here i gave a value for the ID and its Read-Only.
+        public int ID { get; } = 3;
+
+
+        public string Name { set; get; }
+
+    }
+
+       
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsEmployee Emp = new clsEmployee();
+
+            //u can't do that cuz its read only .
+            //Emp.ID = 3;
+
+            Emp.Name = "Maher";
+
+            Console.WriteLine("Name For Employee {0}" , Emp.Name);
+            Console.WriteLine("ID For Employee {0}" , Emp.ID);
+        }
+    }
+}
