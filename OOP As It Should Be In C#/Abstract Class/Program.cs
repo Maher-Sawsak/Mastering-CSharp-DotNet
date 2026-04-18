46d642f7e351a1088391a0803227caea1e4f9a98
Maher-Sawsak <mahersawsak2@gmail.com>
2026-04-18 19:45:13 +0300

    Add Abstract Class concepts and examples

---
+++ OOP As It Should Be In C#/Abstract Class/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Cache;
+using System.Runtime.CompilerServices;
+
+namespace Abstract_Class
+{
+    public abstract class clsPerson {
+        public int Id { get; set; }
+        public int Age  { set; get; }
+
+
+
+        public abstract void FullName();
+    
+    }
+
+    public class clsEmployee: clsPerson{
+
+        public override void FullName()
+        {
+            Console.WriteLine("Hi Done For Abstract");
+        }
+        public void  WorkHours() {
+            Console.WriteLine("The Emp Work for 24/H");
+        }
+    
+    } 
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        clsEmployee Emps1 = new clsEmployee();
+            Emps1.FullName();
+
+        }
+    }
+}
