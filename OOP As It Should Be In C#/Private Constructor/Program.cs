5bc111fccb4db55294f35bbd0fd4c53c2749c799
Maher-Sawsak <mahersawsak2@gmail.com>
2026-04-18 19:45:25 +0300

    Add Private Constructor concepts

---
+++ OOP As It Should Be In C#/Private Constructor/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Private_Constructor
+{
+    class Settings
+    {
+        public static int DayNumber
+        {
+            get
+            {
+                return DateTime.Today.Day;
+            }
+        }
+
+        public static string DayName
+        {
+            get
+            {
+                return DateTime.Today.DayOfWeek.ToString();
+            }
+        }
+
+        public static string ProjectPath
+        {
+            get;
+            set;
+        }
+
+        //this is a private constructor to prevent creating object from this class
+        //there is onther way to prevent creating an object by using static class instead of private consructor.
+        private Settings()
+        {
+
+        }
+
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // You cannot create an object here because class has private constructor
+            // Settings Obj1 = new Settings();
+
+            //
+            // Read the static properties.
+            //
+            Console.WriteLine(Settings.DayNumber);
+            Console.WriteLine(Settings.DayName);
+
+            //
+            // Change the value of the static bool property.
+            //
+            Settings.ProjectPath = @"C:\MyProjects\";
+            Console.WriteLine(Settings.ProjectPath);
+            Console.ReadKey();
+        }
+    }
+}
+
