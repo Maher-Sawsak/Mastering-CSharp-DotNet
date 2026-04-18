7b739ea46108b43adcbdcf7562eb262742fae233
Maher-Sawsak <mahersawsak2@gmail.com>
2026-04-18 19:45:22 +0300

    Add Parameter-less Constructor examples

---
+++ OOP As It Should Be In C#/Parameter-less Constructor/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Parameter_less_Constructor
+{
+
+
+
+    class clsPerson { 
+    
+
+        public int ID { get; set;}
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //Parameterless Constructor
+        public clsPerson() {
+
+            ID = -10;
+            Name = "Maher";
+            Age = 21;
+        }
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        clsPerson Person1 = new clsPerson ();
+
+            Console.WriteLine(Person1.ID);
+            Console.WriteLine(Person1.Name);
+            Console.WriteLine(Person1.Age);
+
+
+
+
+
+
+        }
+    }
+}
