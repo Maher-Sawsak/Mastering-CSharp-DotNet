488e83e317a70976c63e6e3f761f1193475842a3
Maher Sawsak <mahersawsak2@gmail.com>
2026-04-30 12:46:20 +0300

    Add MyFirstClass project

---
+++ OOP As It Should Be In C#/MyFirstClass/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace MyFirstClass
+{
+    public class MyClass
+    {
+
+        public float Sum(int FirstNumber, int SecondNumber)
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public float Sum(int FirstNumber, int SecondNumber, int ThirdNumber)
+        {
+            return FirstNumber + SecondNumber + ThirdNumber;
+        }
+
+
+    }
+}
+
