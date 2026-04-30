b0cd634be14218decfebb545aac14c4dd820addb
Maher Sawsak <mahersawsak2@gmail.com>
2026-04-30 12:46:23 +0300

    Add TestingMyClassLibrary project

---
+++ OOP As It Should Be In C#/TestingMyClassLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MyFirstClass;
+
+namespace TestingMyClassLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyClass Obj = new MyClass();
+                      
+            Console.WriteLine("The First Result  : "  + Obj.Sum(10, 20));
+
+            Console.WriteLine("The Second Result : " + Obj.Sum(10, 20, 20));
+
+           
+
+        }
+    }
+}
