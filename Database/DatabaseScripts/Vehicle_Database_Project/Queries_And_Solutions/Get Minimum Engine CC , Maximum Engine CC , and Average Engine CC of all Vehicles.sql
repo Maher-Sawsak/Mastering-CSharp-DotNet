7e34655d3e698acac68b7fc2e9d56edd3199c2c4
Maher Sawsak <mahersawsak2@gmail.com>
2026-05-29 00:59:37 +0300

    Solve: Calculate Min, Max, and Avg Engine CC of all vehicles

---
+++ Database/DatabaseScripts/Vehicle_Database_Project/Queries_And_Solutions/Get Minimum Engine CC , Maximum Engine CC , and Average Engine CC of all Vehicles.sql
@@ -0,0 +1,4 @@
+--Problem 33: Get Minimum Engine CC , Maximum Engine CC , and Average Engine CC of all Vehicles
+
+Select Max(VehicleDetails.Engine_CC) as MaximumEngineCC,Min(VehicleDetails.Engine_CC) as MinimimEngineCC , Avg(VehicleDetails.Engine_CC) as AverageEngineCC
+from VehicleDetails;
