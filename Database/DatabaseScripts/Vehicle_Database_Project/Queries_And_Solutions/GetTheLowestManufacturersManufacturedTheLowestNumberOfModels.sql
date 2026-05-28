3f6e4bc5b1220159186f01f1af26d195d9480f77
Maher Sawsak <mahersawsak2@gmail.com>
2026-05-29 00:53:05 +0300

    Solve: Get the lowest manufacturers with the lowest number of models

---
+++ Database/DatabaseScripts/Vehicle_Database_Project/Queries_And_Solutions/GetTheLowestManufacturersManufacturedTheLowestNumberOfModels.sql
@@ -0,0 +1,30 @@
+ -- Problem 49: Get the Lowest Manufacturers manufactured the lowest number of models
+
+
+SELECT		 Makes.Make, COUNT(*) AS NumberOfModels
+		FROM   Makes INNER JOIN
+								 MakeModels ON Makes.MakeID = MakeModels.MakeID
+		GROUP BY Makes.Make
+
+		having COUNT(*) = 
+(
+	select MIN(NumberOfModels) as MaxNumberOfModels
+	from
+	(
+
+		SELECT      MakeID, COUNT(*) AS NumberOfModels
+		FROM       
+								 MakeModels
+		GROUP BY MakeID
+		
+	) R1
+
+)
+
+
+
+
+
+
+
+
