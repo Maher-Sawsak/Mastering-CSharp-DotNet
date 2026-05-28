8b3928a9fc7663ee3d432b79bf95b6872f867780
Maher Sawsak <mahersawsak2@gmail.com>
2026-05-29 00:59:27 +0300

    Solve: Get vehicles with OHV engines and 4 cylinders

---
+++ Database/DatabaseScripts/Vehicle_Database_Project/Queries_And_Solutions/Get Make and Vehicles that the engine contains (OHV)and have Cylinders Equal to _4_.sql
@@ -0,0 +1,6 @@
+ -- Problem 25: Get make and vehicles that the engine contains 'OHV' and have Cylinders = 4
+ select  Makes.Make , VehicleDetails.*
+ from VehicleDetails
+ inner join Makes
+ on Makes.MakeID =VehicleDetails.MakeID
+ where ((VehicleDetails.Engine LIKE '%OHV%') And( VehicleDetails.Engine_Cylinders = 4));
