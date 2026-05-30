0c89baeb8782012d42674ff8a7cd584ddf6e9154
Maher Sawsak <mahersawsak2@gmail.com>
2026-05-30 14:28:43 +0300

    feat: add ExecuteScalar example for retrieving single database values efficiently

---
+++ C# & Database Connectivity (ADO.NET)/Retrieve a Single Value (ExecuteScalar)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Retrieve_a_Single_Value__ExecuteScalar_
+{
+    internal class Program
+    {
+        static string ConnectionString = "Server =.;Database= ContactsDB;User Id =sa;Password=MaherSawsak123";
+
+
+        static string GetFirstNameByID(int ContactID) {
+            string FirstName = "";
+            SqlConnection connection = new SqlConnection(ConnectionString);
+
+            string query = "Select FirstName from Contacts where ContactID = @ContactID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("ContactID", ContactID);
+            
+
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+                if (result != null)
+                {
+
+                     FirstName = (string)result;
+
+                }
+                else {
+                     FirstName = "";
+                }
+                
+                connection.Close();
+
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Error {ex.Message}");
+            }
+            return FirstName;
+
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetFirstNameByID(1));
+
+        }
+    }
+}
