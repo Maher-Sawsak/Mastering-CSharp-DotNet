29070bc7fbbf653c6357c5d92ae467ebaf05ca66
Maher Sawsak <mahersawsak2@gmail.com>
2026-05-31 00:59:22 +0300

    feat: retrieve auto-generated ID using ExecuteScalar and SCOPE_IDENTITY

---
+++ C# & Database Connectivity (ADO.NET)/RetrieveAutoNumberAfterInsertingOrAddingData/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+//provider for Sql
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetrieveAutoNumberAfterInsertingOrAddingData
+{
+    internal class Program
+    {
+        static string ConnectionString = "Server =.;Database =ContactsDB;User Id = sa;Password = MaherSawsak123";
+
+        public struct stContactInfo
+        {
+            public string FirstName { set; get; }
+            public string LastName { set; get; }
+            public string Email { set; get; }
+            public string PhoneNumber { set; get; }
+            public string Address { set; get; }
+            public int CountryID { set; get; }
+
+
+        }
+
+
+        static void AddContactInfoAndGetID(stContactInfo ContactInfo) {
+
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            //THIS query contain two part look carefully here......
+            string query = @"INSERT INTO Contacts (FirstName, LastName, Email, Phone, Address, CountryID)
+                            Values(@FirstName, @LastName, @Email, @Phone, @Address, @CountryID);
+                            SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@FirstName", ContactInfo.FirstName);
+            command.Parameters.AddWithValue("@LastName", ContactInfo.LastName);
+            command.Parameters.AddWithValue("@Email", ContactInfo.Email);
+            command.Parameters.AddWithValue("@Phone", ContactInfo.PhoneNumber);
+            command.Parameters.AddWithValue("@Address", ContactInfo.Address);
+            command.Parameters.AddWithValue("@CountryID", ContactInfo.CountryID);
+
+            try
+            {
+
+                connection.Open();
+                object Result = command.ExecuteScalar();
+                //the ExecuteScalar will return data by object so i converted to string then to int,because i know 
+                //query will give me an ID the last ID so if its converted the TryParse will return true if Not false
+                if (Result != null && int.TryParse(Result.ToString(), out int insertID))
+                {
+                    Console.WriteLine($"Newest ID For Insert Is : {insertID}");
+
+                }
+                else
+                {
+                    Console.WriteLine("The ID Is NULL No thing returned ");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error {ex.Message}");
+            }
+            finally {
+                connection.Close();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            stContactInfo ContactRecored = new stContactInfo
+            {
+                FirstName = "Habib",
+                LastName = "Sawsak",
+                Email = "HabibSawsak@gmail.com",
+                PhoneNumber = "0556565398",
+                Address = "Damascus",
+                CountryID = 1,
+            };
+            AddContactInfoAndGetID(ContactRecored);
+
+            //I Got this resutl below ->
+            //Newest ID For Insert Is : 9
+        }
+    }
+}
---
+++ C# & Database Connectivity (ADO.NET)/RetrieveAutoNumberAfterInsertingOrAddingData/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RetrieveAutoNumberAfterInsertingOrAddingData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RetrieveAutoNumberAfterInsertingOrAddingData")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7cc17153-2864-48ca-bf2c-b7cb3384f14e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
