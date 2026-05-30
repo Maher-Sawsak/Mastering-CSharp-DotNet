2b4ea4ca58bc1ef457dcda4d4f5779a2e8db42c3
Maher Sawsak <mahersawsak2@gmail.com>
2026-05-30 11:44:32 +0300

    feat: add SQL Server connection and basic parameterized query example

---
+++ C# & Database Connectivity (ADO.NET)/Connect to SQL Server Database - Parameterized Query/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+
+namespace Connect_to_SQL_Server_Database___Parameterized_Query
+{
+    internal class Program
+    {
+        static string connectionString = "Server =.;Database = ContactsDB;User Id = sa;Password = MaherSawsak123";
+
+        static void PrintAllContactWithFirstName(string FirstName) {
+
+            SqlConnection Connection = new SqlConnection(connectionString);
+            string query = "select * from Contacts where FirstName = @FirstName";
+
+            SqlCommand command = new SqlCommand(query, Connection);
+            command.Parameters.AddWithValue("@FirstName", FirstName);
+
+            try {
+                Connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read()) {
+
+
+                    int ContactID = (int)reader["ContactID"];
+                    string FirstNamee = (string)reader["FirstName"];
+                    string LastName = (string)reader["LastName"];
+                    string Email = (string)reader["Email"];
+                    string Phone = (string)reader["Phone"];
+                    string Address = (string)reader["Address"];
+                    int CountryID = (int)reader["CountryID"];
+
+
+                    Console.WriteLine($"ContactID   :  {ContactID}    ");
+                    Console.WriteLine($"FirstName   :  {FirstNamee}   ");
+                    Console.WriteLine($"LastName    :  {LastName}     ");
+                    Console.WriteLine($"Email       :  {Email}        ");
+                    Console.WriteLine($"Phone       :  {Phone}        ");
+                    Console.WriteLine($"Address     :  {Address}      ");
+                    Console.WriteLine($"CountryID   :  {CountryID}    \n\n");
+
+
+                
+                }
+                //here to close reader and connection.
+                reader.Close();
+                Connection.Close();
+
+
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+           
+            
+
+
+
+
+        }
+
+        static void PrintAllContactWithFirstNameAndCountry(string FirstName, int CountryID)
+        {
+
+            SqlConnection Connection = new SqlConnection(connectionString);
+            string query = "select * from Contacts where FirstName = @FirstName AND CountryID = @CountryID";
+
+            SqlCommand command = new SqlCommand(query, Connection);
+            command.Parameters.AddWithValue("@FirstName", FirstName);
+            command.Parameters.AddWithValue("@CountryID", CountryID);
+
+            try
+            {
+                Connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+
+
+                    int ContactID = (int)reader["ContactID"];
+                    string CurrentFirstName = (string)reader["FirstName"];
+                    string LastName = (string)reader["LastName"];
+                    string Email = (string)reader["Email"];
+                    string Phone = (string)reader["Phone"];
+                    string Address = (string)reader["Address"];
+                    int CurrentCountryID = (int)reader["CountryID"];
+
+
+                    Console.WriteLine($"ContactID   :  {ContactID}    ");
+                    Console.WriteLine($"FirstName   :  {CurrentFirstName}   ");
+                    Console.WriteLine($"LastName    :  {LastName}     ");
+                    Console.WriteLine($"Email       :  {Email}        ");
+                    Console.WriteLine($"Phone       :  {Phone}        ");
+                    Console.WriteLine($"Address     :  {Address}      ");
+                    Console.WriteLine($"CountryID   :  {CurrentCountryID}    \n\n");
+
+
+
+                }
+                //here to close reader and connection.
+                reader.Close();
+                Connection.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+
+
+
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            PrintAllContactWithFirstName("jane");
+            Console.WriteLine("\n\n Second result for FirstName and Country\n\n");
+            PrintAllContactWithFirstNameAndCountry("jane", 1);
+        }
+    }
+}
---
+++ C# & Database Connectivity (ADO.NET)/Connect to SQL Server Database - Parameterized Query/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Connect to SQL Server Database - Parameterized Query")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Connect to SQL Server Database - Parameterized Query")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f1de65e0-ad9d-4a8b-96d1-76462a8ec1e6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
