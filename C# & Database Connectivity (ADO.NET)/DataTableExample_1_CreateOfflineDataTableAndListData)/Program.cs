a6213b893f7da09748cc547ec0782a95bea3de07
Maher Sawsak <mahersawsak2@gmail.com>
2026-06-09 19:56:53 +0300

    feat: add salary statistics calculations to DataTable

--- C# & Database Connectivity (ADO.NET)/DataTableExample_1_CreateOfflineDataTableAndListData)/Program.cs
+++ C# & Database Connectivity (ADO.NET)/DataTableExample_1_CreateOfflineDataTableAndListData)/Program.cs
@@ -20,6 +20,12 @@ namespace DataTableExample_1_CreateOfflineDataTableAndListData_
             dtStudent.Rows.Add(2, "Ali Sawsak", "Syria"   ,32874, DateTime.Now);
             dtStudent.Rows.Add(3, "Habib Sawsak", "Syria" ,1043.3, DateTime.Now);
 
+            int NumberOfStudents  = 0;
+            double SumOfSalary = 0;
+            double AvgSalary = 0;
+            double MinSalary = 0;
+            double MaxSalary = 0;
+                       
 
    
 
@@ -34,6 +40,24 @@ namespace DataTableExample_1_CreateOfflineDataTableAndListData_
                 Console.WriteLine("\t|{0,-4}|{1,-30}|{2,-20}|{3,-10}|{4,-21}|", row["ID"], row["Name"], row["Country"], row["Salary"], row["Date"]);
             }
             Console.WriteLine("\t|_________________________________________________________________________________________|");
+    
+        NumberOfStudents = dtStudent.Rows.Count;
+        SumOfSalary = Convert.ToDouble(dtStudent.Compute("Sum(Salary)",string.Empty));
+        AvgSalary = Convert.ToDouble(dtStudent.Compute("Avg(Salary)",string.Empty));
+        MinSalary = Convert.ToDouble(dtStudent.Compute("Min(Salary)",string.Empty));
+        MaxSalary = Convert.ToDouble(dtStudent.Compute("Max(Salary)",string.Empty));
+
+
+            Console.WriteLine($"Number Of Students : {NumberOfStudents}");
+            Console.WriteLine($"Sum of Salary : {SumOfSalary}");
+            Console.WriteLine($"Avg of Salary : {AvgSalary}");
+            Console.WriteLine($"Min of Salary : {MinSalary}");
+            Console.WriteLine($"Max of Salary : {MaxSalary}");
+
+
+        
+        
         }
     }
+
 }
