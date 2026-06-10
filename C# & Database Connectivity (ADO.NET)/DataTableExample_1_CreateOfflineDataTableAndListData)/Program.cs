a319324eb054ca416cbcf685704db6487495e691
Maher Sawsak <mahersawsak2@gmail.com>
2026-06-10 09:27:37 +0300

    feat: add filtering and compute logic to DataTable

--- C# & Database Connectivity (ADO.NET)/DataTableExample_1_CreateOfflineDataTableAndListData)/Program.cs
+++ C# & Database Connectivity (ADO.NET)/DataTableExample_1_CreateOfflineDataTableAndListData)/Program.cs
@@ -19,6 +19,7 @@ namespace DataTableExample_1_CreateOfflineDataTableAndListData_
             dtStudent.Rows.Add(1, "Maher Sawsak", "Turkey",2092.3, DateTime.Now);
             dtStudent.Rows.Add(2, "Ali Sawsak", "Syria"   ,32874, DateTime.Now);
             dtStudent.Rows.Add(3, "Habib Sawsak", "Syria" ,1043.3, DateTime.Now);
+            dtStudent.Rows.Add(3, "Nour Sawsak", "KSA" ,324, DateTime.Now);
 
             int NumberOfStudents  = 0;
             double SumOfSalary = 0;
@@ -55,8 +56,58 @@ namespace DataTableExample_1_CreateOfflineDataTableAndListData_
             Console.WriteLine($"Max of Salary : {MaxSalary}");
 
 
-        
-        
+
+
+            //Now Applying the filter for table .
+            DataRow[] DataRows;
+            DataRows = dtStudent.Select("Country = 'Syria' or Country ='KSA'");
+            Console.WriteLine("\nFilter For Syria (OR) KSA Country : ");
+            foreach (DataRow RecordRow in DataRows)
+            {
+                Console.WriteLine($"ID :{RecordRow["ID"]}\tName: {RecordRow["Name"]}\tCountry : {RecordRow["Country"]} ");
+            }
+
+
+
+
+
+       
+            DataRows =  dtStudent.Select("Country = 'Syria'");
+            Console.WriteLine("\nFilter For Syria Country Only: ");
+            foreach (DataRow  RecordRow in DataRows) 
+            {
+                Console.WriteLine($"ID :{RecordRow["ID"]}\tName: {RecordRow["Name"]}\tCountry : {RecordRow["Country"]} ");
+            }
+
+
+            Console.WriteLine("\nAdding Filter Compute Function On Syria Country");
+
+            NumberOfStudents = DataRows.Count();
+            SumOfSalary = Convert.ToDouble(dtStudent.Compute("Sum(Salary)", "Country='Syria'"));
+            AvgSalary = Convert.ToDouble(dtStudent.Compute("Avg(Salary)", "Country='Syria'"));
+            MinSalary = Convert.ToDouble(dtStudent.Compute("Min(Salary)", "Country='Syria'"));
+            MaxSalary = Convert.ToDouble(dtStudent.Compute("Max(Salary)", "Country='Syria'"));
+
+
+            Console.WriteLine($"Number Of Students : {NumberOfStudents}");
+            Console.WriteLine($"Sum of Salary : {SumOfSalary}");
+            Console.WriteLine($"Avg of Salary : {AvgSalary}");
+            Console.WriteLine($"Min of Salary : {MinSalary}");
+            Console.WriteLine($"Max of Salary : {MaxSalary}");
+
+
+            //Show Only Student Hold ID 1.
+            //IT WILL ONLY HOLD ONE RECORD THERE IS NO NEED TO ADD []
+            DataRows = dtStudent.Select("ID=1");
+            Console.WriteLine("\nFiltering : Student With ID = 1 : ");
+            if (DataRows.Length > 0)
+            {
+                Console.WriteLine($"ID: {DataRows[0]["ID"]}\tName: {DataRows[0]["Name"]}\tCountry: {DataRows[0]["Country"]}");
+            }
+            else
+            {
+                Console.WriteLine("Student not found.");
+            }
         }
     }
 
