43bbc457e4a68b97ef598fdf3178bae4572ffe2d
Maher Sawsak <mahersawsak2@gmail.com>
2026-06-11 09:36:40 +0300

    feat: demonstrate DataTable.Clear() method to remove all rows

---
+++ C# & Database Connectivity (ADO.NET)/Datatable (Clear)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datatable__Clear_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            DataTable dtEmp = new DataTable();
+
+            dtEmp.Columns.Add("ID", typeof(int));
+
+            dtEmp.Columns.Add("Name", typeof(string));
+
+            dtEmp.Columns.Add("Country", typeof(string));
+
+
+
+
+
+            dtEmp.Rows.Add(1, "Maher Sawsak", "Syria");
+
+            dtEmp.Rows.Add(2, "Ali Sawsak", "Turkey");
+
+            dtEmp.Rows.Add(3, "Habib Sawsak", "Turkey");
+
+            dtEmp.Rows.Add(4, "Nidal Sawsak", "Saudi Arabia");
+
+
+
+
+
+            foreach (DataRow RecordRow in dtEmp.Rows)
+            {
+                Console.WriteLine($"ID: {RecordRow["ID"]}\tName: {RecordRow["Name"]}\t Country: {RecordRow["Country"]}");
+            }
+
+            
+            
+            //This is how to delete all data in data table.
+            dtEmp.Clear();
+
+            Console.WriteLine("\n\nAfter Using Clear Method : ");
+            foreach (DataRow RecordRow in dtEmp.Rows)
+            {
+                Console.WriteLine($"ID: {RecordRow["ID"]}\tName: {RecordRow["Name"]}\t Country: {RecordRow["Country"]}");
+            }
+
+        }
+    }
+}
---
+++ C# & Database Connectivity (ADO.NET)/Datatable (Clear)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Datatable (Clear)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Datatable (Clear)")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6b4b29ce-dbfa-413d-91be-d209aa954df8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
