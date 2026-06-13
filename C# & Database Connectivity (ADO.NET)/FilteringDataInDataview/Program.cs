b2fe8962e2da1465f68b945e3810f3178ddbb6a3
Maher Sawsak <mahersawsak2@gmail.com>
2026-06-13 21:51:43 +0300

    DataView Filtering Data and print it.

---
+++ C# & Database Connectivity (ADO.NET)/FilteringDataInDataview/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilteringDataInDataview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable EmployeesDataTable = new DataTable();
+
+            EmployeesDataTable.Columns.Add("ID", typeof(int));
+            EmployeesDataTable.Columns.Add("Name", typeof(string));
+            EmployeesDataTable.Columns.Add("Country", typeof(string));
+            EmployeesDataTable.Columns.Add("Salary", typeof(Double));
+            EmployeesDataTable.Columns.Add("Date", typeof(DateTime));
+
+            //Add rows
+            EmployeesDataTable.Rows.Add(1, "Seren", "Jordan", 5000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(2, "Ali Maher", "Syria", 525.5, DateTime.Now);
+            EmployeesDataTable.Rows.Add(3, "Lina Kamal", "Jordan", 730.5, DateTime.Now);
+            EmployeesDataTable.Rows.Add(4, "Fadi JAmeel", "Egypt", 800, DateTime.Now);
+            EmployeesDataTable.Rows.Add(5, "Omar Mahmoud", "Lebanon", 7000, DateTime.Now);
+
+            Console.WriteLine("\nEmployees List:\n");
+
+            foreach (DataRow RecordRow in EmployeesDataTable.Rows)
+            {
+                //Using Field Name
+                Console.WriteLine(" ID: {0}\t Name : {1} \t Country: {2} \t Salary: {3} Date: {4} \t ", RecordRow["ID"], RecordRow["Name"], RecordRow["Country"], RecordRow["Salary"], RecordRow["Date"]);
+            }
+
+            DataView DataView1 = new DataView();
+            DataView1 = EmployeesDataTable.DefaultView;
+            DataView1.RowFilter = "Country='Jordan' or Country='Syria'";
+
+            Console.WriteLine("\n\nPrinting View After Filtering");
+            for (int i = 0; i < DataView1.Count; i++) 
+            {
+                Console.WriteLine($"Name : {DataView1[i][1]} \tCountry :{DataView1[i][2]}");
+            }
+
+
+        }
+    }
+}
