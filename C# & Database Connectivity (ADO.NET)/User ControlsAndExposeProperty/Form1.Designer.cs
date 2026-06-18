65ad69b4c134e1339f057edd77ef792c84d6796c
Maher Sawsak <mahersawsak2@gmail.com>
2026-06-18 21:59:47 +0300

    feat :   User Controls Expose Property

---
+++ C# & Database Connectivity (ADO.NET)/User ControlsAndExposeProperty/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace User_ControlsAndExposeProperty
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.ctlrSimpleCalculater2 = new User_ControlsAndExposeProperty.ctlrSimpleCalculater();
+            this.ctlrSimpleCalculater1 = new User_ControlsAndExposeProperty.ctlrSimpleCalculater();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(572, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show Result";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ctlrSimpleCalculater2
+            // 
+            this.ctlrSimpleCalculater2.Location = new System.Drawing.Point(38, 238);
+            this.ctlrSimpleCalculater2.Name = "ctlrSimpleCalculater2";
+            this.ctlrSimpleCalculater2.Size = new System.Drawing.Size(292, 200);
+            this.ctlrSimpleCalculater2.TabIndex = 1;
+            // 
+            // ctlrSimpleCalculater1
+            // 
+            this.ctlrSimpleCalculater1.Location = new System.Drawing.Point(38, 21);
+            this.ctlrSimpleCalculater1.Name = "ctlrSimpleCalculater1";
+            this.ctlrSimpleCalculater1.Size = new System.Drawing.Size(261, 195);
+            this.ctlrSimpleCalculater1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ctlrSimpleCalculater2);
+            this.Controls.Add(this.ctlrSimpleCalculater1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctlrSimpleCalculater ctlrSimpleCalculater1;
+        private ctlrSimpleCalculater ctlrSimpleCalculater2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ C# & Database Connectivity (ADO.NET)/User ControlsAndExposeProperty/ctlrSimpleCalculater.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace User_ControlsAndExposeProperty
+{
+    partial class ctlrSimpleCalculater
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 24);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(204, 24);
+            this.textBox2.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(111, 164);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(48, 25);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "???";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "+";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(87, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 24);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ctlrSimpleCalculater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "ctlrSimpleCalculater";
+            this.Size = new System.Drawing.Size(299, 201);
+            this.Load += new System.EventHandler(this.ctlrSimpleCalculater_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
